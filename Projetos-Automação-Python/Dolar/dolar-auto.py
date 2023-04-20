1ca9da7d32fd2badfca87b4d07fd61071d42d94e
aleyssongarcia <aleyssongarcia@gmail.com>
2023-04-20 01:01:31 -0300

    dolar RPA

---
+++ Projetos-Automação-Python/Dolar/dolar-auto.py
@@ -0,0 +1,78 @@
+import pyautogui as posicaoMouse
+import pyautogui as tempoEspera
+
+# Tempo de espera para que o computador possa pensar e processar.
+tempoEspera.sleep(2)
+
+# Movento o mouse ate o botão iniciar (x,y)
+posicaoMouse.moveTo(557, 1052)
+
+
+# Tempo de espera para que o computador possa pensar e processar.
+tempoEspera.sleep(0)
+
+
+# Clicando na posição do botão
+posicaoMouse.click(557, 1052)
+
+# Tempo de espera para que o computador possa pensar e processar.
+tempoEspera.sleep(0)
+
+# Digitando o nome do navegador Google Chrome
+posicaoMouse.typewrite('Google Chrome')
+
+# Tempo de espera para que o computador possa pensar e processar.
+tempoEspera.sleep(0)
+
+# Precionando a tecla enter para abrir o google chrome.
+posicaoMouse.press('enter')
+
+
+# Tempo de espera para que o computador possa pensar e processar.
+tempoEspera.sleep(3)
+
+# Digitando a pesquisa de dolar.
+posicaoMouse.typewrite('Dolar hoje')
+
+# Tempo de espera para que o computador possa pensar e processar.
+tempoEspera.sleep(0)
+
+# Precionando a tecla enter para pesquisar o dolar.
+posicaoMouse.press('enter')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Movento o mouse ate o aplicativo da calculadora(x,y)
+# posicaoMouse.moveTo(747, 350)
+
+# # Tempo de espera para que o computador possa pensar e processar.
+# tempoEspera.sleep(0)
+
+# # Clicando na posição do botão da calculadora.
+# posicaoMouse.click(747, 350)
+
+# # Tempo de espera para que o computador possa pensar e processar.
+# tempoEspera.sleep(0)
+
+# # Digitando o nome do navegador Google Chrome
+# posicaoMouse.typewrite('Dolar hoje')
+
+# # Tempo de espera para que o computador possa pensar e processar.
+# tempoEspera.sleep(0)
