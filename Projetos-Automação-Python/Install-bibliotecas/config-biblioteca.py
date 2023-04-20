59ee6b10dda449446e71b4fd38806d8722dcff62
aleyssongarcia <aleyssongarcia@gmail.com>
2023-04-20 02:00:26 -0300

    configurando biblioteca

---
+++ Projetos-Automação-Python/Install-bibliotecas/config-biblioteca.py
@@ -0,0 +1,16 @@
+from selenium import webdriver as opcoesSelenium
+from selenium.webdriver.common.by import By
+
+navegador = opcoesSelenium.Chrome()
+
+navegador.get('https://www.google.com/')
+
+
+
+
+# instalações utilizadas para desenvolvimento destas aplicações.
+
+# pip install selenium
+# pip install openpyxl
+# pip install pandas
+# pip install Pyinstaller
